791a4019c60609b011d859f961a6494009dc1573
Simona <s.c.dulgheru@gmail.com>
2019-10-21 11:13:50 +0100

    Password Generator. Copy Button inactive

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Password-Generator
+
+Generates a memorable password.
+
+The user is able to generate the desired password length between 8 and 128 characters. 
+Password length:
+Must contain 8 to 128 characters.
+
+
+Characters included in the password:
+Will contain letters(UpperCase and LowerCase)
+Will contain numbers, special characters and symbols. 
+
+
+PREVIEW
+
+![Password Generator main page](https://user-images.githubusercontent.com/48987979/67195051-292c9900-f3f0-11e9-9c04-8c42916181aa.jpg)
+
+![Password requirements](https://user-images.githubusercontent.com/48987979/67195070-334e9780-f3f0-11e9-87fc-3e33154aaba0.jpg)
+
+![Generated password](https://user-images.githubusercontent.com/48987979/67195016-19ad5000-f3f0-11e9-9579-6e65310a9620.jpg)
+
+
+This project will be updated with a responsive web application.
+
+
+
+
---
+++ script.js
@@ -0,0 +1,39 @@
+
+
+
+
+const charPass = `abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"#$%&'()*+,-./:;<=>?@[ \ ]^_{|}~`;
+
+let minChar = 8;
+let maxChar = 128;
+let userInput;
+let strLength;
+
+const generateBtn = document.querySelector(`#generate`);
+const textArea = document.querySelector(`#gen-password`);
+const copyBtn = document.querySelector(`#copy`);
+
+generateBtn.addEventListener(`click`, function () {
+    userInput = prompt(`Please select your desired password length. Type a number between 8 and 128. Password will include numbers, letters and special characters.`);
+
+    while (Number(userInput) < minChar || Number(userInput) > maxChar) {
+        userInput = prompt(`Please select your password. Your password needs to be between 8 and 128 characters`);
+    }
+    return randomPassword();
+});
+
+
+function randomPassword() {
+
+    let strLength = Number(userInput);
+    let pass = ``;
+    for (let x = 0; x < strLength; x++) {
+        let i = Math.floor(Math.random() * charPass.length);
+        pass += charPass.charAt(i);
+    }
+    document.querySelector("#gen-password").innerHTML = (pass);
+};
+randomPass();
+
+
+
